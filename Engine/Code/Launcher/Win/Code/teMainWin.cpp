02b160000823bb69af92b8edef1a292c250db20c
l.vadim <vadimluchko@gmail.com>
2014-03-27 11:42:49 +0200

    stativ vars friendly leaks detections

--- Engine/Code/Launcher/Win/Code/teMainWin.cpp
+++ Engine/Code/Launcher/Win/Code/teMainWin.cpp
@@ -90,10 +90,12 @@ int CrtReportHook(int nRptType, char *szMsg, int *retVal)
 int main(int argc, char * argv[])
 {
 #ifdef TE_COMPILER_MSVC
-	_CrtSetReportHook (CrtReportHook);
-	_CrtMemState s1, s2, s3;
-	_CrtMemCheckpoint(&s1);
+ _CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
+ _CrtSetReportHook (CrtReportHook);
+ _CrtMemState s1, s2, s3;
+ _CrtMemCheckpoint(&s1);
 #endif
+
 
 #else
 
@@ -165,7 +167,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	if(_CrtMemDifference(&s3, &s1, &s2))
 	{
 		_CrtMemDumpStatistics(&s3);
-		_CrtDumpMemoryLeaks();
+		//_CrtDumpMemoryLeaks();
 	}
 #endif
 
